fd8a83c98f1b276395385ac694f858a0aeacfa77
T1meLord1 <vlad.bobrenok.2001@gmail.com>
2024-01-19 13:42:50 +0300

    Создан полностью готовый калькулятор, бесконечно повторяющий свой запрос на ввод числа пользователем до того как он введёт 5. Использован System.exit(0) вместо break; поскольку break используется только в циклах

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -8,38 +8,68 @@ public class Main {
 
     public static void main(String[] args) {
         //todo Вам нужно дополнить методы так, чтобы получился рабочий калькулятор!
-        System.out.println("Что вы хотите сделать? \n1-разделить\n2-отнять\n3-умножить\n4-сложить");
+        while (true) {
+            System.out.println("Что вы хотите сделать? \n1-разделить\n2-отнять\n3-умножить\n4-сложить\n5-закрыть калькулятор");
+            inputText();
+            if (input == 1) {
+                divide();
+            }
+            if (input == 2) {
+                diff();
+            }
+            if (input == 3) {
+                multiply();
+            }
+            if (input == 4) {
+                sum();
+            }
+            if (input == 5) {
+                System.exit(0);
+            }
+        }
+        }
 
-    }
+        private static void divide () {
+            inputDouble();
+            c = a / b;
+            System.out.println("При делении " + a + " на " + b + " получается число: " + c);
+            //todo дописать логику и печать результата
+        }
 
-    private static void divide() {
-        c = a / b;
-        //todo дописать логику и печать результата
-    }
+        private static void diff () {
+            inputDouble();
+            c = a - b;
+            System.out.println("При вычитании " + a + " от " + b + " получается число: " + c);
+            //todo дописать логику и печать результата
+        }
 
-    private static void diff() {
-        c = a - b;
-        //todo дописать логику и печать результата
-    }
+        private static void multiply () {
+            inputDouble();
+            c = a * b;
+            System.out.println("При умножении " + a + " на " + b + " получается число: " + c);
+            //todo дописать логику и печать результата
+        }
 
-    private static void multiply() {
-        c = a * b;
-        //todo дописать логику и печать результата
-    }
+        private static void sum () {
+            inputDouble();
+            c = a + b;
+            System.out.println("При сложении " + a + " на " + b + " получается число: " + c);
+            //todo дописать логику и печать результата
+        }
 
-    private static void sum() {
-        c = a + b;
-        //todo дописать логику и печать результата
-    }
+        private static double inputDouble () {
+            System.out.println("Введите первое число: ");
+            a = new Scanner(System.in).nextInt();
+            System.out.println("Введите второе число: ");
+            b = new Scanner(System.in).nextInt();
+            //todo дописать логику
+            return 0;
 
-    private static double inputDouble() {
-        System.out.println("Введите ваш ответ: ");
-        input = new Scanner(System.in).nextInt();
-        System.out.println("Введите первое число: ");
-        a = new Scanner(System.in).nextInt();
-        System.out.println("Введите второе число: ");
-        b = new Scanner(System.in).nextInt();
-        //todo дописать логику
-        return 0;
-    }
+        }
+
+        private static void inputText () {
+            System.out.println("Введите ваш ответ: ");
+            input = new Scanner(System.in).nextInt();
+        }
+
 }
