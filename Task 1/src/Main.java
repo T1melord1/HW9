000775d1a63b989ea45e02e9ca5a46d96861d0f2
T1meLord1 <vlad.bobrenok.2001@gmail.com>
2024-01-19 12:25:47 +0300

    Дописываю логику, добавлена переменная "С" для записи введенных данных пользователя и изменен метод inputeDoble, но пока не доделан

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -1,35 +1,44 @@
+import java.util.Scanner;
+
 public class Main {
     static double a = 0;
     static double b = 0;
+    static double input = 0;
+    static double c = 0;
 
     public static void main(String[] args) {
         //todo Вам нужно дополнить методы так, чтобы получился рабочий калькулятор!
-        a = inputDouble();
-        b = inputDouble();
+        System.out.println("Что вы хотите сделать? \n1-разделить\n2-отнять\n3-умножить\n4-сложить");
 
-        sum();      //Метод суммирования чисел
-        multiply(); //Метод умножения чисел
-        diff();     //Метод вычитания чисел
-        divide();   //Метод деления чисел
     }
 
     private static void divide() {
+        c = a / b;
         //todo дописать логику и печать результата
     }
 
     private static void diff() {
+        c = a - b;
         //todo дописать логику и печать результата
     }
 
     private static void multiply() {
+        c = a * b;
         //todo дописать логику и печать результата
     }
 
     private static void sum() {
+        c = a + b;
         //todo дописать логику и печать результата
     }
 
     private static double inputDouble() {
+        System.out.println("Введите ваш ответ: ");
+        input = new Scanner(System.in).nextInt();
+        System.out.println("Введите первое число: ");
+        a = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число: ");
+        b = new Scanner(System.in).nextInt();
         //todo дописать логику
         return 0;
     }
